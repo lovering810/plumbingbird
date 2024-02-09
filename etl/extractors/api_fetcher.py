a710da7b70e65df68b2bc2f3632c56b504aa451b
Rebecca Lovering <lovering810@gmail.com>
2024-02-09 16:42:03 -0500

    Docs and TODOs

--- etl/extractors/api_fetcher.py
+++ etl/extractors/api_fetcher.py
@@ -29,7 +29,7 @@ class APIFetcher(Fetcher):
 
     def connect(self) -> requests.Session:
         """
-        Connect to API with basic auth.
+        Connect to API with basic auth if creds are supplied
         return: requests.Session
         """
         session = requests.Session()
@@ -65,16 +65,27 @@ class APIStreamFetcher(APIFetcher):
     def parse_response(self, response_json: dict) -> Any:
         """
         Parse JSON of response for whatever is relevant to this fetcher.
-        Defaults to returning the entire response_json.
+        Intended to be overridden in child classes by bizlogic.
+        Parent class defaults to returning the entire response_json.
         """
         return response_json
 
     def fetch_iter(self, batch_size: int = 100) -> Iterator[dict[str, Any]]:
+        """
+        Pages through a JSON-returning API, applying self.parse_response()
+        to each response and returning results as an Iterator.
+
+        :param batch_size int: how many results to request per page, defaults to 100
+        """
+
         session = self.connect()
         current_index = 0
         batch_size = 100
         while True:
             with session as sesh:
+                # TODO: expand the customization of the request
+                # include additional params as necessary, rather than URL
+                # manipulation only.
                 response = sesh.get(self.compose_url(batch_size, current_index))
                 response.raise_for_status()
 
--- etl/transformers/csv_streamer.py
+++ etl/transformers/csv_streamer.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Any, Iterator
+from typing import Iterator
 
 from ...utilities.etl_primitives import Buffer
 from ...utilities.streamer import StringIteratorIO
@@ -11,12 +11,28 @@ class CSVBuffer(Buffer):
         super().__init__(iter, keymap_loc)
 
     @staticmethod
-    def clean_value(value: Any = None) -> str:
+    def clean_value(value: str = None) -> str:
+        """
+        CSV-specific string revision for values to ensure null values and
+        newlines in content are correctly handled.
+
+        :param value str: value to be a CSV cell
+        :return str: CSV-friendly version of the value
+        """
         if value is None:
             return r"\N"
         return str(value).replace("\n", "\\n")
 
     def process(self, field_name: str, field_value: str):
+        """
+        Generic function for using field_name to assign logic
+        to apply to a field_value.
+        Defaults to returning the same value that is given.
+
+        :param field_name str: name of the field in the results to which this value belongs.
+        :param field_value str: value of field for this record
+        """
+        # TODO: genericize/build out
         match field_name:
             case "id":
                 if field_value == "id":
@@ -27,6 +43,14 @@ class CSVBuffer(Buffer):
                 return field_value
 
     def get_buffer(self, fields: list, delim: str = "`") -> Iterator:
+        """
+        Wraps the class input iterator (of dicts) in additional processing and
+        formatting to make it behave like a CSV.
+
+        :param fields list: list of fields for each record to include in the
+        output CSV
+        :param delim str: the delimiter to use in the output CSV, defaults to "`"
+        """
         item_iterator = StringIteratorIO(
             (
                 delim.join(
